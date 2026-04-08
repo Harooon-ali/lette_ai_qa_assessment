ef0c87eb3a8ccf535da1c79e4e438fe7f3424289
Harooon-ali <100220412+Harooon-ali@users.noreply.github.com>
2026-04-08 17:46:58 +0100

    Update validate_data.py

--- data_validation/validate_data.py
+++ data_validation/validate_data.py
@@ -1,1 +1,18 @@
+import json
 
+data = {
+    "users":[
+        {"id":1,"email":"test1@test.com","country":"IE"},
+        {"id":2,"email":"test2@test.com","country":"US"}
+    ],
+    "orders":[
+        {"id":101,"userId":1,"amount":100},
+        {"id":102,"userId":3,"amount":200}
+    ]
+}
+
+users = {u["id"] for u in data["users"]}
+
+for order in data["orders"]:
+    if order["userId"] not in users:
+        print("Invalid user reference:", order)
